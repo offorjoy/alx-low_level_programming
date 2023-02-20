296f6263e842d110df9dd58f641daa80929e2fde
offorjoy <124433730+offorjoy@users.noreply.github.com>
2023-02-20 23:01:35 +0100

    Create 5-printf .c

---
+++ 5-printf .c
@@ -0,0 +1,6 @@
+julien@ubuntu:~/c/0x00$ gcc -Wall -Werror -Wextra -pedantic -std=gnu89 5-printf.c
+julien@ubuntu:~/c/0x00$ ./a.out 
+with proper grammar, but the outcome is a piece of art,
+julien@ubuntu:~/c/0x00$ echo $?
+0
+julien@ubuntu:~/c/0x00$ 
