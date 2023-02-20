2a42d0927a0a78c1e9b710ddfecf1cb62d11912b
offorjoy <124433730+offorjoy@users.noreply.github.com>
2023-02-20 22:58:30 +0100

    Create 4-puts.c

---
+++ 4-puts.c
@@ -0,0 +1,5 @@
+julien@ubuntu:~/c/0x00$ gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out
+"Programming is like building a multilingual puzzle
+julien@ubuntu:~/c/0x00$ echo $?
+0
+julien@ubuntu:~/c/0x00$ 
